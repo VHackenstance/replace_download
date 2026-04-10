e828eae5494ccdb3576d1fb9b0dbe1f31b5214fb
kali <hackenstance@proton.me>
2026-04-10 11:54:50 -0500

    Detect if .exe file being downloaded, find response to request

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 <h4>Intercept a download and replace it with my own file.</h4>
 <h4>I guess it will work locally but let's see remote</h4>
 <h4>Create iptables queue for input and output</h4>
-<p>iptables -I INPUT -j NFQUEUE --queue-num 0</p>
-<p>iptables -I OUTPUT -j NFQUEUE --queue-num 0</p>
+<p>sudo iptables -I INPUT -j NFQUEUE --queue-num 0 --queue-bypass</p>
+<p>sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0 --queue-bypass</p>
 <ol>
 <li><b></b>:</li>
 <li><b></b>:</li>
--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import netfilterqueue
 from scapy.layers.inet import IP, TCP
 from scapy.layers.dns import Raw
 
+ack_list = []
+
 def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
     if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
-        print("Packet has Raw and TCP layers.")
         if scapy_packet[TCP].dport == 80:
             print("HTTP Request: ")
-            print(scapy_packet.show())
+            if ".exe" in scapy_packet[Raw].load:
+                # Does not work with https, which is all sites now
+                print("[+] Exe Request: ")
+                # get the acknowledgement (ack) reference of the request.
+                ack_list.append(scapy_packet[TCP].ack)
+                print(scapy_packet.show())
+                # manually initiate TCP handshake
+                # some code here to replace the code
         elif scapy_packet[TCP].sport == 80:
             print("HTTP Response: ")
-            print(scapy_packet.show())
+            # Look for the sequence of the current response is in the ack list
+            if scapy_packet[TCP].sec in ack_list:
+                ack_list.remove(scapy_packet[TCP].seq)
+                print("[+] Replacing File: ")
+                print(scapy_packet.show())
 
     packet.accept()
     return None # suppress excess packets being printed.
