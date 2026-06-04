1b3d33f810e855065fc624d21c6685ec61b1ef3f
Vhackenstance <hackenstance@proton.me>
2026-06-04 10:40:23 -0500

    Rebuild: Filter Traffic Based on Port Used.

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 <h3>REPLACE DOWNLOAD</h3>
-<h4>Intercept a download and replace it with my own file.</h4>
-<h4>I guess it will work locally but let's see remote</h4>
-<h4>Create iptables queue for input and output</h4>
-<p>sudo iptables -I INPUT -j NFQUEUE --queue-num 0 --queue-bypass</p>
-<p>sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0 --queue-bypass</p>
+<h4>Intercept Target download and replace it with Spoof file.</h4>
+<h4>Create iptables queue</h4>
 <p>
     <b>
         <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">
--- main.py
+++ main.py
@@ -7,8 +7,21 @@ from scapy.layers.dns import Raw
 def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
     if scapy_packet.haslayer(Raw):
+        if scapy_packet[TCP].dport == 80:
+            print("\n\n[+] This is a HTTP Request: ")
+            print(scapy_packet.show())
+        elif scapy_packet[TCP].sport == 80:
+            print("\n\n[+] This is a HTTP Response: ")
+            print(scapy_packet.show())
+        # Do not know why as of now but OWASP Juice Shop on P:3000 is serving requests http
+        if scapy_packet[TCP].dport == 443:
+            print("\n\n[+] This is a HTTPS Request: ")
+            print(scapy_packet.show())
+        elif scapy_packet[TCP].sport == 443:
+            print("\n\n[+] This is a HTTPS Response: ")
+            print(scapy_packet.show())
 
-        packet.accept()
+    packet.accept()
 
 if __name__ == "__main__":
     queue = netfilterqueue.NetfilterQueue()
