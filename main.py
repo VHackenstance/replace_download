764ec36efd2ee086cc8ac88779e78796063be69b
Vhackenstance <hackenstance@proton.me>
2026-06-05 09:04:05 -0500

    Filter request with exe, retrieve the ack and seq numbers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scapy.layers.inet import IP, TCP
 from scapy.layers.dns import Raw
 
 extensions = [".exe",".url",".zip",".pdf",".json",".md",".yml","","","",]
+ack_list = []
 
 def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
@@ -12,36 +13,41 @@ def process_packet(packet):
         if scapy_packet.haslayer(TCP):
             print("\n[+] Packet has layer TCP")
             if scapy_packet[TCP].dport == 80:
-                print("[+] This is a HTTP Request: \n")
-                load = str(scapy_packet[Raw].load)
-                print("[+] Load set to a string:")
-                # ****** This works, for online OWASP Juice Shop ******
-                print(load)
+                print("[+] This is a HTTP Request:  ")
+                if ".exe" in scapy_packet[Raw].load:
+                    print("[+] Found an Exe Request:  ")
+                    print("[+] Here is our Acknowledgement Number: ")
+                    print(scapy_packet[TCP].ack)
+                    ack_list.append(scapy_packet[TCP].ack)
+
+            elif scapy_packet[TCP].sport == 80:
+                print("[+] This is a HTTP Response:  ")
+                if scapy_packet[TCP].seq:
+                    print("[+] Here is our Sequence Number: ")
+                    print(scapy_packet[TCP].seq)
+                    if scapy_packet[TCP].seq in ack_list:
+                        print("[+] Replacing File: ")
+
+
 
             # ********** TODO
             # ONE. search HTTP load now for .exe
             # TWO. scrap but keep commented code for HTTPS
             # ********** END TODO
 
-            # This does not work!!!!!!!!!!!!!!!!!!!!!!!!!
-            # for local OWASP so giving it a miss for now.
-            # elif scapy_packet[TCP].dport == 443:
-            #     print("[+] This is a HTTPS Request: \n")
-            #     print(scapy_packet[Raw].load)
+            # print(scapy_packet.show())
+            # load = str(scapy_packet[Raw].load)
+            # print("[+] Load set to a string:")
+            # print(load)
 
-
-
-        # Look for an .exe download, from but can update this for any download
-        # elif scapy_packet[TCP].sport == 80:
-        #     print("\n\n[+] This is a HTTP Response: ")
-        #     print(scapy_packet.show())
-        # # Do not know why as of now but OWASP Juice Shop on P:3000 is serving requests http
-        # if scapy_packet[TCP].dport == 443:
-        #     print("\n\n[+] This is a HTTPS Request: ")
-        #     print(scapy_packet.show())
-        # elif scapy_packet[TCP].sport == 443:
-        #     print("\n\n[+] This is a HTTPS Response: ")
-        #     print(scapy_packet.show())
+            # ********** This does not work! For local OWASP port 3000
+            # Cannot unencrypt load, which shows as Broken binary.
+            # if scapy_packet[TCP].dport == 443:
+            #     print("\n\n[+] This is a HTTPS Request: ")
+            #     print(scapy_packet.show())
+            # elif scapy_packet[TCP].sport == 443:
+            #     print("\n\n[+] This is a HTTPS Response: ")
+            #     print(scapy_packet.show())
 
     packet.accept()
 
